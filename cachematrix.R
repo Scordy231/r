ee95b7a8fab5f318b26a87dd39b7f2d011538461
Samuel Adebayo <samueladebayo50@gmail.com>
2016-08-31 18:01:37 +0100

    first commit

---
+++ cachematrix.R
@@ -0,0 +1,31 @@
+MatrixInverse
+
+makeCacheMatrix <- function(x = matrix()) {
+  invrs <- NULL
+  set <- function(y) {
+    x <- y
+    invrs <- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) invrs <- inverse
+  getinverse <- function() invrs
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
+}
+
+
+Matrixreturn
+
+cacheSolve <- function(x, ...) {
+  invrs <= x$getinverse()
+  if(!is.null(invrs)) {
+    message("getting cached data")
+    return(invrs)
+  }
+  mat <- x$get()
+  invrs <- solve(mat, ...)
+  x$setinverse(invrs)
+  invrs
+}
